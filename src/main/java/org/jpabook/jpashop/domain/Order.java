611ed6b406d5ec4797b7aa78ae5c45c864d4b1bb
wjcho <wj930303@gmail.com>
2024-07-07 16:29:59 +0900

    Feat: Order에 addOrderItem 메서드 추가

--- src/main/java/org/jpabook/jpashop/domain/Order.java
+++ src/main/java/org/jpabook/jpashop/domain/Order.java
@@ -30,6 +30,11 @@ public class Order {
     @Enumerated(EnumType.STRING)
     private OrderStatus status;
 
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
     public Long getId() {
         return id;
     }
