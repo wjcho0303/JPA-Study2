7f2f9a99eb6ac3c099dc70bc38945d8754f62a2d
wjcho <wj930303@gmail.com>
2024-07-11 14:53:54 +0900

    Fix: 임베디드 객체를 불변 객체로 만들기.

--- src/main/java/org/jpabook/jpashop/domain/JobPeriod.java
+++ src/main/java/org/jpabook/jpashop/domain/JobPeriod.java
@@ -20,16 +20,8 @@ public class JobPeriod {
         return startDate;
     }
 
-    public void setStartDate(LocalDateTime startDate) {
-        this.startDate = startDate;
-    }
-
     public LocalDateTime getEndDate() {
         return endDate;
-    }
-
-    public void setEndDate(LocalDateTime endDate) {
-        this.endDate = endDate;
     }
 
     public JobPeriod() {
